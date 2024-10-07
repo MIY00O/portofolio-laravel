cc349d6d3eb76ca2333aab0a3354d95e28ae96d7
Miyo Suzuya <damchongkun@gmail.com>
2024-10-07 15:47:44 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,32 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-
-    public function index(): View
-    {
-        $post = Post::latest()->paginate(10);
-        return view('post.index', compact('product'));
-    }
-
-    public function create(): View
-    {
-        return view('post.create');
-    }
-
-    public function store(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'image'         => 'required|image|mimes:png,jpg|max:2024',
-            'title'         => 'required|min:6',
-            'description'   => 'required',
-        ]);
-        $image = $request->file()
-    }
+    protected $fillable = [
+        'title',
+        'image',
+        'description',
+    ];
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,14 +3,36 @@
 namespace App\Models;
 
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class Post extends Model
 {
-    protected $filltable =
-    [
-        'image',
-        'title',
-        'desciption',
-    ];
+    public function create(): View
+    {
+        return view('post.create');
+    }
+
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'title'         => 'required|min:6',
+            'description'   => 'required|min:10',
+            'image'         => 'required|image|mimes:png,jpeg,jpg|max:2048',
+        ]);
+        $image = $request->file('image');
+        $imageName = $image->hashName();
+        $image->move(storage_path('app/public/products'), $imageName);
+
+        Product::create([
+            'image'         => $imageName,
+            'title'         => $request->title,
+            'desciption'    => $request->desciption,
+        ]);
+        return redirect()->route('Post.index');
+    }
 }
